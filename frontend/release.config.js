1d28217b1bba0754819eb82957b00745b337e8c5
Tamás Juhász <tamasjuhasz84@yahoo.com>
2025-07-05 15:06:50 +0200

    fix(frontend): change workflow fix(backend): change workflow

---
+++ frontend/release.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  branches: ['master'],
+  tagFormat: 'frontend-v${version}',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+}
