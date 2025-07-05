6609df38aeca3d01a069f08b339853fbd238eeb5
Tamás Juhász <tamasjuhasz84@yahoo.com>
2025-07-05 15:25:49 +0200

    fix(frontend): test changelog separation

--- frontend/release.config.js
+++ frontend/release.config.js
@@ -1,15 +1,36 @@
 export default {
   branches: ['master'],
-  tagFormat: 'frontend-v${version}',
+  tagFormat: 'v${version}',
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'angular',
+      },
+    ],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: './CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
     [
       '@semantic-release/git',
       {
         assets: ['CHANGELOG.md', 'package.json'],
-        message: 'chore(frontend): ${nextRelease.version} [skip ci]',
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
     '@semantic-release/github',
